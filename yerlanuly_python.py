4b25c2115501bf219244b4e730df9adafaf577ec
Yerassyl <erasik01@gmail.com>
2024-11-06 23:42:25 +0500

    task uploaded

---
+++ yerlanuly_python.py
@@ -0,0 +1,43 @@
+# Task 1
+
+
+number = int(input())
+
+if number > 7:
+    print("Hello")
+
+
+name = input()
+
+if name == "John":
+    print("Hello, John")
+else:
+    print("There is no such name")
+
+
+array = list(map(int, input().split()))
+multiplies_of_3 = [x for x in array if x % 3 == 0]
+print(*multiplies_of_3)
+
+
+# Task 2
+
+"""
+[((())()(())]]
+
+a) Can this sequence be considered correct? - No
+
+b) If the answer to the previous question is “no”, 
+then what needs to be changed in it to make it correct?
+
+- This sequence needs one closing bracket ')' and 
+one opening square bracket '[' so the sequence would look like:
+   [()(())()(())[]]
+        or
+   [[((()))()(())]]
+"""
+
+
+
+
+
